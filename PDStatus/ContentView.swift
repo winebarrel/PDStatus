a82380c9370abc959b1212002a5367ef08d59010
winebarrel <sugawara@winebarrel.jp>
2024-01-06 10:27:26 +0900

    first commit

---
+++ PDStatus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDStatus
+//
+//  Created by 菅原元気 on 2024/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDStatus/PDStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDStatusApp.swift
+//  PDStatus
+//
+//  Created by 菅原元気 on 2024/01/06.
+//
+
+import SwiftUI
+
+@main
+struct PDStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
